2ef3a8ad002fd1bf53615df383151dee3a842357
ericagill <erica.gilioli@gmail.com>
2023-11-02 20:44:38 +0000

    responsive design and projects routing

---
+++ src/data/ProjectData.js
@@ -0,0 +1,34 @@
+const residential = [
+    {
+        title: "Private Residence London",
+        img1: "", 
+        img2:"",
+        description:"Entire refurbishment for the existing building from the early twenties to the moder area. New extension family room, new garden and pool design.",
+    },
+    {
+        title: "Notting Hill Maisonette",
+        img1: "", 
+        img2:"",
+        description:"The property comprises of two floors.The ground floor offer an inviting living room benefits from wooden floors, feature large windows create a warm environment with natural light. Separate fitted kitchen and ample additional storage.The first floor includes three bedrooms,contemporary family bathroom and an additional one for guests.The client wanted to refurbish the property completely. The configuration of the kitchen is now different then the previouse one.",
+    },
+    {
+        title: "Lambeth Victorian House",
+        img1: "", 
+        img2:"",
+        description:"A big house in Lambeth borough, compose by three floors is one of my project in London. The kitchen based on the lower ground floor, next to the garden, needs to be updated. I worked on several different configurations to find the perfect one to reach the client’s expectations, needs and future plans. The environment will be expanded over the next two years. The final configuration is the best option in order to add eventually in the future walls and based units, keeping the same design. All the base units are hanging on the wall except for the island that is on glass legs. The configuration of all the existing appliances has changed, finishes, tiles and flooring too. The refurbishment has been completed..",
+    },
+    {
+        title: "Clerkenwell Penthouse",
+        img1: "", 
+        img2:"",
+        description:"A refurbish building in Clerckenwell was recently completed by a penthouse.Two apartment, one and two bedroom flats, needed fitted kitchens.The two bedroom flat has a nice space for the kitchen which is an open plan on the big living area.Neutral colours for the cabinets, black and white, with a breakfast bar table on top of the island, in oak finishes let people entertain guests while cooking, surrounded by an exceptional view of London. The one bedroom flat has a very big space for the kitchen: again an open space plan on the living area, low ceiling with windows complete the modern space.",
+    },
+    {
+        title: "South Kensington apartment",
+        img1: "", 
+        img2:"",
+        description:"Amazing space in South Kensington for this three bedrooms apartment. Reception room with French windows allow the light to reach all the space making it warm and comfortable. Used as a vacation mansion, the client needed to have a simple and elegant taste at the same time for the dining area and TV space. All white and natural oak wood elements, with closed units and shelves, keep every element tidy. For the kids bedrooms, same style and functionality, creating furniture to maximize the space for the kids to play and enjoy theenvironment.",
+    },
+]
+
+export  default residential;
